cf520967e137c672a279dfce285ae92cd819a753
Rob McDonald <rob@layermade.com>
2015-07-03 18:24:28 +0100

    Anki tests
    - Added anki folder for flashcard code
    - Added index code examples
    - Added sublime-project file

---
+++ anki/indexes-02.py
@@ -0,0 +1,36 @@
+#
+# Indexes: functions
+#
+
+## Append: .append()
+####################
+
+### 1
+
+letters = ['a', 'b', 'c']
+letters.append('d')
+print len(letters)
+print letters
+
+#### Q: What does len() do here
+#### Q: What happens to letters list when we .append('d')
+
+
+### 2
+
+suitcase = [] 
+suitcase.append("sunglasses")
+
+# Your code here!
+suitcase.append("suncream")
+suitcase.append("swimming costume")
+suitcase.append("book")
+
+# list_length = ________ # Set this to the length of suitcase
+
+print "There are %d items in the suitcase." % (list_length)
+print suitcase
+
+#### Q: How do you set an empty list (array)
+#### Q: How do we set the `list_length`?
+#### Q: What does %d print out?
