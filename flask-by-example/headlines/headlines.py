9d86ccecf44de330d87d3e8e2e7afb3154479c1e
Rob <rob@layermade.com>
2016-11-29 17:13:28 +0000

    Update weather and articles to separate functions to display home.html function

--- flask-by-example/headlines/headlines.py
+++ flask-by-example/headlines/headlines.py
@@ -10,6 +10,7 @@ from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
+
 RSS_FEEDS = {
     'bbc': 'http://feeds.bbci.co.uk/news/rss.xml',
     'cnn': 'http://rss.cnn.com/rss/edition.rss',
@@ -17,26 +18,43 @@ RSS_FEEDS = {
     'iol': 'http://www.iol.co.za/cmlink/1.640'
 }
 
+DEFAULTS = {
+    'publication': 'bbc',
+    'city': 'London, UK'
+}
+
 
 @app.route('/')
-def get_news():
-    query = request.args.get('publication')
-    if not query or query.lower() not in RSS_FEEDS:
-        publication = 'bbc'
-    else:
-        publication = query.lower()
-    feed = feedparser.parse(RSS_FEEDS[publication])
-    weather = get_weather()
+def home():
+    # get customized headlines, based on user input or default
+    publication = request.args.get('publication')
+    if not publication:
+        publication = DEFAULTS['publication']
+    articles = get_news(publication)
+    # get customized weather based on user input or default
+    city = request.args.get('city')
+    if not city:
+        city = DEFAULTS['city']
+    weather = get_weather(city)
     return render_template(
         'home.html',
-        articles=feed['entries'],
+        articles=articles,
         weather=weather
     )
 
 
-def get_weather():
+def get_news(query):
+    if not query or query.lower() not in RSS_FEEDS:
+        publication = DEFAULTS['publication']
+    else:
+        publication = query.lower()
+    feed = feedparser.parse(RSS_FEEDS[publication])
+    return feed['entries']
+
+
+def get_weather(query):
     payload = {
-        'q': 'London, uk',
+        'q': query,
         'units': 'metric',
         'appid': 'dd5001c4843d408c7a4ed6fc8b490e04'
     }
@@ -46,12 +64,15 @@ def get_weather():
     )
     parsed = r.json()
     weather = None
-    # if r.status_code == requests.codes.ok:
+
+    # Simplify the original json response
+    # - r.status_code is better (to check if successfully parsed)
     if parsed.get('weather'):
         weather = {
             'description': parsed['weather'][0]['description'],
             'temperature': parsed['main']['temp'],
-            'city': parsed['name']
+            'city': parsed['name'],
+            'country': parsed['sys']['country']
         }
     return weather
 
