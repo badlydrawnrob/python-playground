597732bb7c80936729b78919eaa0912abf4363c6
Rob <rob@layermade.com>
2016-11-29 16:20:54 +0000

    Add in weather using requests module

--- flask-by-example/headlines/headlines.py
+++ flask-by-example/headlines/headlines.py
@@ -1,4 +1,11 @@
+"""
+    HEADLINES
+    ~~~~~~~~~
+    Flask by example
+"""
+
 import feedparser
+import requests
 from flask import Flask, render_template, request
 
 app = Flask(__name__)
@@ -19,9 +26,34 @@ def get_news():
     else:
         publication = query.lower()
     feed = feedparser.parse(RSS_FEEDS[publication])
+    weather = get_weather()
     return render_template(
-        'home.html', articles=feed['entries']
+        'home.html',
+        articles=feed['entries'],
+        weather=weather
     )
+
+
+def get_weather():
+    payload = {
+        'q': 'London, uk',
+        'units': 'metric',
+        'appid': 'dd5001c4843d408c7a4ed6fc8b490e04'
+    }
+    r = requests.get(
+        'http://api.openweathermap.org/data/2.5/weather',
+        params=payload
+    )
+    parsed = r.json()
+    weather = None
+    # if r.status_code == requests.codes.ok:
+    if parsed.get('weather'):
+        weather = {
+            'description': parsed['weather'][0]['description'],
+            'temperature': parsed['main']['temp'],
+            'city': parsed['name']
+        }
+    return weather
 
 
 if __name__ == '__main__':
