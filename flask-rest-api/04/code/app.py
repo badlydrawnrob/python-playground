8aa3b797de8b3dca31fb2ec89c04d0fd5040043b
Rob <rob@layermade.com>
2017-04-11 13:00:48 +0200

    Convert item to proper `get_json()` method, using Flask `request`

--- flask-rest-api/04/code/app.py
+++ flask-rest-api/04/code/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify
+from flask import Flask, request
 from flask_restful import Resource, Api
 
 app = Flask(__name__)
@@ -15,10 +15,8 @@ class Item(Resource):
         return {'item': None}, 404  # Not found
 
     def post(self, name):
-        item = {
-            'name': name,
-            'price': 12.00
-        }
+        data = request.get_json()
+        item = {'name': name, 'price': data['price']}
         items.append(item)
         return item, 201  # Created
 
