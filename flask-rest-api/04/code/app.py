9f4f31cf59ceb4af34902f15dff7a0fe124da688
Rob <rob@layermade.com>
2017-04-11 17:38:25 +0200

    Add a conditional check to return a 400 error if name already exists. The client should be checking this anyway, but this acts as a safeguard

--- flask-rest-api/04/code/app.py
+++ flask-rest-api/04/code/app.py
@@ -13,10 +13,15 @@ class Item(Resource):
         return {'item': item}, 200 if item else 404
 
     def post(self, name):
-        data = request.get_json()
-        item = {'name': name, 'price': data['price']}
-        items.append(item)
-        return item, 201  # Created
+        # The client should really check if name exists before, so use
+        # 400: bad request
+        if next(filter(lambda x: x['name'] == name, items), None) is not None:
+            return {'message': 'An item with name {} already exists.'.format(name)}, 400
+        else:
+            data = request.get_json()
+            item = {'name': name, 'price': data['price']}
+            items.append(item)
+            return item, 201  # Created code
 
 
 class ItemList(Resource):
