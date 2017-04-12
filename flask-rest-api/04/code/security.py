a179acc6c693fa859d407fe1063e29157d905f0d
Rob <rob@layermade.com>
2017-04-12 11:54:39 +0200

    Tidy up code with a User() class and set() comprehensions

--- flask-rest-api/04/code/security.py
+++ flask-rest-api/04/code/security.py
@@ -1,30 +1,12 @@
+from user import User
+
 users = [
-    {
-        'id': 1,
-        'username': 'bob',
-        'password': 'asdf'
-    }
+    User(1, 'bob', 'asdf')
 ]
 
-# Mapping allows us to find a user
-# without having to loop through the list
-# username_mapping['bob']
+username_mapping = {u.username: u for u in users}
+userid_mapping = {u.id: u for u in users}
 
-username_mapping = {
-    'bob': {
-        'id': 1,
-        'username': 'bob',
-        'password': 'asdf'
-    }
-}
-
-userid_mapping = {
-    1: {
-        'id': 1,
-        'username': 'bob',
-        'password': 'asdf'
-    }
-}
 
 def authenticate(username, password):
     user = username_mapping.get(username, None)
