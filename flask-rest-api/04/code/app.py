a5178f4c624bf55839a6a3d50582446192d10244
Rob <rob@layermade.com>
2017-04-12 12:52:56 +0200

    Add flask_jwt, fix `filter()` variable

--- flask-rest-api/04/code/app.py
+++ flask-rest-api/04/code/app.py
@@ -1,21 +1,26 @@
 from flask import Flask, request
 from flask_restful import Resource, Api
+from flask_jwt import JWT, jwt_required
+
+from security import authenticate, identity
 
 app = Flask(__name__)
 app.secret_key = 'jose'
 api = Api(app)
 
+jwt = JWT(app, authenticate, identity) # /auth
+
 items = []
 
 
 class Item(Resource):
+    @jwt_required()
     def get(self, name):
-        item = next(filter(lambda x: item['name'] == name, items), None)
+        item = next(filter(lambda x: x['name'] == name, items), None)
         return {'item': item}, 200 if item else 404
 
     def post(self, name):
-        # The client should really check if name exists before, so use
-        # 400: bad request
+        # The client should really check if name exists before
         if next(filter(lambda x: x['name'] == name, items), None):
             return {'message': 'An item with name {} already exists.'.format(name)}, 400
 
