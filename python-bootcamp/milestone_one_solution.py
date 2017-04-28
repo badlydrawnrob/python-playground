353978f2c913d83bacc3f2ec633d08384574f1c2
Rob <rob@layermade.com>
2017-04-28 11:44:16 +0200

    Change a few functions to better represent docs, add a couple of functions as well as calling some of the functions with dummy data

--- python-bootcamp/milestone_one_solution.py
+++ python-bootcamp/milestone_one_solution.py
@@ -1,3 +1,6 @@
+from random import randint
+
+
 keys = [1,2,3,4,5,6,7,8,9]
 board = [0,0,0,0,0,0,0,0,0]
 
@@ -21,7 +24,7 @@ def player_input():
     marker = ''
 
     while not (marker == 'O' or marker == 'X'):
-        marker = input('Player 1: Do you want to be X or O? ')
+        marker = input('Player 1: Do you want to be X or O? ').upper()
 
     if marker == 'X':
         return ('X', 'O')
@@ -29,27 +32,27 @@ def player_input():
         return ('O', 'X')
 
 
-def mark_board(board, marker, position):
+def place_marker(board, marker, position):
     board[position - 1] = marker
-    return board
 
 
-def check_board(board):
-    winner = None
+# != win_check is missing a `mark` argument
 
-    for row in horizontal_board(board):
-        if ['X', 'X', 'X'] in row:
-            winner = ('X', 'O')
-        elif ['O', 'O', 'O'] in row:
-            winner = ('X', 'O')
+def win_check(board):
+    board = horizontal_board(board)
 
-    if winner == ('X', 'O') or winner == ('O', 'X'):
-        return winner
+    if ['X', 'X', 'X'] in board:
+        return True, 'X'
+    elif ['O', 'O', 'O'] in board:
+        return True, 'O'
+    return False
+
+
+def choose_first():
+    if randint(0, 1) == 0:
+        return 'Player 2'
     else:
-        return False
-
-    print(horizontal_board)
-    print(vertical_board)
+        return 'Player 1'
 
 
 def space_check(board, position):
@@ -57,29 +60,52 @@ def space_check(board, position):
 
 
 def full_board_check(board):
-    for i in board:
-        if isinstance(i, int) and space_check(board, i):
-            return False
-        elif 'X' or 'O':
-            print(i)
+    # We don't loop through the board itself,
+    # but use range() and our space_check()
+    # function, passing the index of i
+
+    for i in range(9):
+        if space_check(board, i):
             return False
     return True
 
 
-def play_game():
-    player_order = player_input()
+def player_choice(board):
+    # Assign variable before conditional checks
+    position = 0
+    positions_allowed = [str(n) for n in range(1,10)]
+
+    while int(position) not in positions_allowed or not space_check(board, int(position)):
+        position = input('Choose your next position: (1-9) ')
+    return int(position)
+
+
+# def play_game():
+#     player_order = player_input()
 
 
 # print(play_game())
+print(choose_first())
+# print(player_choice(board))
+
 print(space_check(board, 1))
-print(mark_board(board, 'X', 1))
+print(place_marker(board, 'X', 1))
 print(full_board_check(board)) # String causes error
-print(check_board(board))
+print(win_check(board))
 
 print(space_check(board, 2))
-print(mark_board(board, 'X', 2))
+print(place_marker(board, 'X', 2))
 print(full_board_check(board)) # String causes error
-print(check_board(board))
+print(win_check(board))
+
+print(space_check(board, 3))
+print(place_marker(board, 'X', 3))
+print(full_board_check(board)) # String causes error
+print(win_check(board))
+
+new_board = ['X','X','X','X','X','X','X','X','X']
+print(horizontal_board(new_board))
+print(win_check(new_board))
 
 print(horizontal_board(board))
 print(vertical_board(board))
