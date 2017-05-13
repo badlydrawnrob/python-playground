a8310c52f2f5ef5bbaef413579cebb6ab07d0387
Rob <rob@layermade.com>
2017-05-13 17:17:28 +0200

    Use backup of stages to rejig memory. Prepare for next step

--- flask-rest-api/04/code/app.py
+++ flask-rest-api/04/code/app.py
@@ -1,16 +1,25 @@
 from flask import Flask
 from flask_restful import Resource, Api
 
-
 app = Flask(__name__)
 api = Api(app)
 
+items = []
 
-class Student(Resource):
+
+class Items(Resource):
     def get(self, name):
-        return {'student': name}
+        for item in items:
+            if item['name'] == name:
+                return item, 200
+        return {'item': None}, 404
+
+    def post(self, name):
+        item = {'name': name, 'price': 12.99}
+        items.append(item)
+        return item, 201
 
 
-api.add_resource(Student, '/student/<string:name>')
+api.add_resource(Items, '/item/<string:name>')
 
-app.run(port=5000)
+app.run(port=5000, debug=True)
