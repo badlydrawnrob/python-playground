38510bf4737fb075d15bb2a4a85ef8ecf238cf8f
Rob <rob@layermade.com>
2017-05-14 19:55:14 +0200

    Move noted version to /tests, create new version with correct imports etc for security to work

--- flask-rest-api/04/code/tests/app_05.py
+++ flask-rest-api/04/code/tests/app_05.py
@@ -1,17 +1,32 @@
 from flask import Flask, request
 from flask_restful import Resource, Api
+
+# Import JWT and our security functions
 from flask_jwt import JWT, jwt_required
 from security import authenticate, identity
 
 app = Flask(__name__)
 app.secret_key = 'asdf'
 api = Api(app)
-jwt = JWT(app, authenticate, identity)
+
+# Initialise the JWT object
+# pass the `security` functions
+# - Send it a username and password
+#
+# 1. authenticate generates the token
+# 2. when we send a token, it:
+#    - calls the identity function,
+#    - checks token
+#    - and gets the user id
+#    - returning correct user
+jwt = JWT(app, authenticate, identity) # /auth
 
 items = []
 
 
 class Item(Resource):
+    # Decorator forces authentication
+    # - requires `Authorization` header with JWT token
     @jwt_required()
     def get(self, name):
         item = next(filter(lambda x: x['name'] == name, items), None)
@@ -19,7 +34,7 @@ class Item(Resource):
 
     def post(self, name):
         if next(filter(lambda x: x['name'] == name, items), None):
-            return {'message': '{} already exists'.format(name)}, 400
+            return {'message': 'An item with name {} already exists'.format(name)}, 400
         data = request.get_json()
         item = {'name': name, 'price': data['price']}
         items.append(item)
