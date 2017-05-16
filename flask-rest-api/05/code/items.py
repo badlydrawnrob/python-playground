02cf3d3295e607a8bca2b6b651c30c136de2356f
Rob <rob@layermade.com>
2017-05-16 17:47:58 +0200

    Add a way to add an item to the database, checking first if it already exists!

--- flask-rest-api/05/code/items.py
+++ flask-rest-api/05/code/items.py
@@ -1,3 +1,8 @@
+'''
+CRUD:
+Create, Read, Update, Delete
+'''
+
 import sqlite3
 from flask_restful import Resource, reqparse
 from flask_jwt import jwt_required
@@ -14,6 +19,19 @@ class Item(Resource):
 
     @jwt_required()
     def get(self, name):
+        # Pull out the db query into a
+        # separate function ...
+        #
+        # - You can call this by using `self`
+        # - OR `(Classname).find...`
+        item = self.find_by_name(name)
+        if item:
+            return item
+        return {'message': 'Item not found'}, 404
+
+
+    @classmethod
+    def find_by_name(cls, name):
         connection = sqlite3.connect('data.db')
         cursor = connection.cursor()
 
@@ -22,19 +40,32 @@ class Item(Resource):
         row = result.fetchone()
         connection.close()
 
-        # Check if `row` contains a result
         if row:
-            return {'item': {'name': row[0], 'price': row[1]}}, 200
-        return {'message': 'Item not found'}, 404
+            return {'item': {'name': row[0], 'price': row[1]}}
 
 
     def post(self, name):
-        if next(filter(lambda x: x['name'] == name, items), None):
-            return {'{} already exists'.format(name)}
+        # You can call a classmethod
+        # - `Item.find_by_name`
+        # - `self.find_by_name`
+        if self.find_by_name(name):
+            return {'message': '{} already exists'.format(name)}, 400
 
         data = Item.parser.parse_args()
         item = {'name': name, 'price': data['price']}
-        items.append(item)
+
+        # Remove the items.append() method
+        # - Add in the database method
+
+        connection = sqlite3.connect('data.db')
+        cursor = connection.cursor()
+
+        query = "INSERT INTO items VALUES (?, ?)"
+        cursor.execute(query, (item['name'], item['price']))
+
+        connection.commit()
+        connection.close()
+
         return item, 201
 
 
