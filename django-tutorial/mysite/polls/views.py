10f2c6ab98a7426bc8ef019099f8a4d1531422ea
Rob <rob@layermade.com>
2017-10-06 16:33:14 +0100

    Using their standard 404 try/except (the previous version did work, but misspelled the template file, must be same name as method)

--- django-tutorial/mysite/polls/views.py
+++ django-tutorial/mysite/polls/views.py
@@ -1,6 +1,6 @@
+from django.http import Http404
 from django.shortcuts import render
 from django.http import HttpResponse
-# from django.template import loader
 
 from .models import Question
 
@@ -11,9 +11,11 @@ def index(request):
     return render(request, 'polls/index.html', context)
 
 def detail(request, question_id):
-    question = Question.objects.get(id=question_id)
-    context = {'question': question}
-    return render(request, 'polls/detail.html', context)
+    try:
+        question = Question.objects.get(pk=question_id)
+    except Question.DoesNotExist:
+        raise Http404("Question does not exist")
+    return render(request, 'polls/detail.html', {'question': question})
 
 def results(request, question_id):
     response = "You're looking at the results of question {}"
