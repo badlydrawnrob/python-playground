910b2b8be073b50b17fa5e3282285460d282aa4b
Rob <rob@layermade.com>
2017-10-15 09:20:22 +0100

    Add blog section

--- django-wagtail/mysite/blog/models.py
+++ django-wagtail/mysite/blog/models.py
@@ -4,10 +4,10 @@ from wagtail.wagtailcore.models import Page
 from wagtail.wagtailcore.fields import RichTextField
 from wagtail.wagtailadmin.edit_handlers import FieldPanel
 
-
-class HomePage(Page):
-    body = RichTextField(blank=True)
+# Create your models here.
+class BlogIndexPage(Page):
+    intro = RichTextField(blank=True)
 
     content_panels = Page.content_panels + [
-        FieldPanel('body', classname="full")
+        FieldPanel('intro', classname="full")
     ]
