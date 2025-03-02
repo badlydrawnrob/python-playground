614b82bbd1374364bc1b9a08b4a719e29346c5e9
Rob <hello@robmcdonald.me>
2025-03-02 20:58:25 +0000

    Update jwt_handler.py

--- building-with-fast-api/chapter_07/auth/jwt_handler.py
+++ building-with-fast-api/chapter_07/auth/jwt_handler.py
@@ -1,4 +1,12 @@
 # ------------------------------------------------------------------------------
 # JWT Token handler
 # ==============================================================================
-# Contains functions to encode and decode the JWT strings
+# Contains functions to encode and decode the JWT strings. JWTs are signed with
+# a secret key known only to the sender and the reciever, but it might be visible
+# to outside users if using a javascript frontend.
+#
+# A JWT is an encoded string usually containing a dictionary housing:
+#
+# 1. A payload
+# 2. A signature
+# 3. It's algorithm
